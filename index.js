309b50ccb867f0332e4561b6b42584aa960b5442
Nemanja <miticnemanja223@gmail.com>
2025-07-09 18:56:04 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+app.listen(port);
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "n81058538@gmail.com",
+    pass: `${process.env.MAIL_PASSWORD}`,
+  },
+});
+
+app.post("/enquiry", (req, res) => {
+  const mailOptions = {
+    from: "n81058538@gmail.com",
+    to: "miticnemanja223@gmail.com",
+    subject: `${req.body.nameSurname}`,
+    text: `${req.body.message}\n\n${req.body.email}\n${req.body.phone}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.error("Error:", error);
+    }
+  });
+});
